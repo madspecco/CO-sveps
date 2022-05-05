2f782c3ffa25962c6e2134c2343d0c6be2a91c95
madspecco <102137035+madspecco@users.noreply.github.com>
2022-05-05 12:32:57 +0300

    Add files via upload
    Made slight changes to UI

--- ProiectDigitsPi/src/main/java/com/example/proiectdigitspi/Main.java
+++ ProiectDigitsPi/src/main/java/com/example/proiectdigitspi/Main.java
@@ -7,13 +7,17 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
+    public void start(Stage primaryStage) throws Exception
+    {
+        FXMLLoader loader = new FXMLLoader(new File("C:/UPT/Second Year/Second Semester/CO/Project/ProiectDigitsPi/src/main/resources").toURI().toURL());
+        Parent root = loader.load();
+        //Parent root = FXMLLoader.load(getClass().getResource("C:/UPT/Second Year/Second Semester/CO/Project/ProiectDigitsPi/src/main/resources"));
         primaryStage.setTitle("CPU Benchmark");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
